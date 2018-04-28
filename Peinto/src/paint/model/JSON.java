a186a8a84da2f7a27969b7a6d886b6b592c3e1a4
Mayssara Omar <mayssaraomar@gmail.com>
2018-04-28 09:50:15 +0200

    Fixed bugs in saving and moving

--- Peinto/src/paint/model/JSON.java
+++ Peinto/src/paint/model/JSON.java
@@ -145,71 +145,88 @@ public class JSON implements SaveAndLoad {
 
 	@Override
 	public void save(String path, ArrayList<Shape> shapes) {
-		// TODO Auto-generated method stub
 
-		int i;
-		JSONObject jBigObject = new JSONObject();
-		jBigObject.put("noOfShapes", "" + shapes.size());
-		JSONObject jShapes;
+		JSONObject jsonBigObject = new JSONObject();
+		jsonBigObject.put("ShapesSize", Integer.toString(shapes.size()));
 
-		for (i = 0; i < shapes.size(); i++) {
-			Shape s = shapes.get(i);
-			jShapes = new JSONObject();
-			jShapes.put("Name", s.getClass().getCanonicalName());
-			jShapes.put("PositionX", s.getPosition().getX());
-			jShapes.put("PositionY", s.getPosition().getY());
-			jShapes.put("Color", s.getColor().getRGB());
-			if (s.getFillColor() != null)
-				jShapes.put("FillColor", s.getFillColor().getRGB());
-			else
-				jShapes.put("FillColor", null);
-			jShapes.put("properties", s.getProperties());
-
-			jBigObject.put("" + i, jShapes);
+		for (Shape shape : shapes) {
+			JSONObject jsonShape = getJSONShape(shape);
+			jsonBigObject.put(Integer.toString(shapes.size() - 1), jsonShape);
 		}
+		writeJSONObjectToFile(path, jsonBigObject);
+
+	}
+
+	private JSONObject getJSONShape(Shape shape) {
+		JSONObject JSONShape = new JSONObject();
+
+		JSONShape.put("Name", shape.getClass().getCanonicalName());
+		JSONShape.put("PositionX", shape.getPosition().getX());
+		JSONShape.put("PositionY", shape.getPosition().getY());
+		JSONShape.put("FillColor", (shape.getFillColor() == null) ? null : shape.getFillColor().getRGB());
+		JSONShape.put("Color", (shape.getColor() == null) ? null : shape.getColor().getRGB());
+		JSONShape.put("Properties", shape.getProperties());
+
+		return JSONShape;
+	}
+
+	private void writeJSONObjectToFile(String path, JSONObject jsonBigObject) {
+
 		try {
-			FileWriter file = new FileWriter(path);
-			file.write(jBigObject.toJSONString());
-			file.close();
+			FileWriter fileWriter = new FileWriter(path);
+			fileWriter.write(jsonBigObject.toJSONString());
+			fileWriter.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
 	}
 
 	@Override
 	public ArrayList<Shape> load(String path) {
-		JSONParser jsonParser = new JSONParser();
-		ArrayList<Shape> shapes = new ArrayList<Shape>();
 
+		ArrayList<Shape> shapes = new ArrayList<Shape>();
+		JSONParser jsonParser = new JSONParser();
 		try {
 			File file = new File(path);
 			Object object = jsonParser.parse(new FileReader(file));
 			JSONObject jsonObject = (JSONObject) object;
-			int size = Integer.parseInt(((String) jsonObject.get("noOfShapes")));
+			int shapesSize = Integer.parseInt(((String) jsonObject.get("ShapesSize")));
 
-			for (int i = 0; i < size; i++) {
-				JSONObject jShapes = (JSONObject) jsonObject.get(i + "");
-				Class o = null;
+			for (int i = 0; i < shapesSize; i++) {
+				JSONObject JSONShape = (JSONObject) jsonObject.get(Integer.toString(i));
+				Class<?> c = null;
 				try {
-					o = Class.forName((String) jShapes.get("Name"));
+					c = Class.forName((String) JSONShape.get("Name"));
 				} catch (Exception e) {
 				}
-				if (o != null) {
-					Shape s = (Shape) o.newInstance();
-					s.setColor(new Color(((Number) jShapes.get("Color")).intValue()));
-					if (jShapes.get("FillColor") != null)
-						s.setFillColor(new Color(((Number) jShapes.get("FillColor")).intValue()));
-					s.setProperties((Map<String, Double>) jShapes.get("properties"));
-					s.setPosition(new Point(((Number) jShapes.get("PositionX")).intValue(),
-							((Number) jShapes.get("PositionY")).intValue()));
-					shapes.add(s);
-				}
+				if (c != null) {
+					shapes.add(getShape(c, JSONShape));
 
+				}
 			}
 		} catch (Exception e) {
 		}
 		return shapes;
 	}
 
+	Shape getShape(Class<?> c, JSONObject JSONShape) {
+		Shape shape = null;
+		try {
+			shape = (Shape) c.newInstance();
+
+			shape.setFillColor((JSONShape.get("FillColor") == null) ? null
+					: new Color(((Number) JSONShape.get("FillColor")).intValue()));
+			shape.setColor(
+					(JSONShape.get("Color") == null) ? null : new Color(((Number) JSONShape.get("Color")).intValue()));
+			shape.setProperties((Map<String, Double>) JSONShape.get("Properties"));
+			shape.setPosition(new Point(((Number) JSONShape.get("PositionX")).intValue(),
+					((Number) JSONShape.get("PositionY")).intValue()));
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+		return shape;
+
+	}
 }
